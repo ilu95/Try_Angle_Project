76a921a0f3190c43610c36f6ab97ccc0a432cee4
ilu95 <44719087+ilu95@users.noreply.github.com>
2023-05-07 06:16:43 +0900

    TensorFlow Lite Object Detection

---
+++ app/build/tmp/kapt3/stubs/debug/org/tensorflow/lite/examples/objectdetection/ImageUtils.java
@@ -0,0 +1,17 @@
+package org.tensorflow.lite.examples.objectdetection;
+
+@kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000\u0018\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u0012\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u00c6\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u000e\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u0006\u00a8\u0006\u0007"}, d2 = {"Lorg/tensorflow/lite/examples/objectdetection/ImageUtils;", "", "()V", "imageToNV21", "", "image", "Landroid/media/Image;", "app_debug"})
+public final class ImageUtils {
+    @org.jetbrains.annotations.NotNull()
+    public static final org.tensorflow.lite.examples.objectdetection.ImageUtils INSTANCE = null;
+    
+    private ImageUtils() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final byte[] imageToNV21(@org.jetbrains.annotations.NotNull()
+    android.media.Image image) {
+        return null;
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/org/tensorflow/lite/examples/objectdetection/fragments/CameraFragment.java
@@ -0,0 +1,95 @@
+package org.tensorflow.lite.examples.objectdetection.fragments;
+
+@kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000\u0098\u0001\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\b\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0006\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010!\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\t\n\u0002\b\b\u0018\u00002\u00020\u00012\u00020\u0002B\u0005\u00a2\u0006\u0002\u0010\u0003J\u0018\u0010\u0019\u001a\u00020\u001a2\u0006\u0010\u001b\u001a\u00020\u001c2\u0006\u0010\u001d\u001a\u00020\u001cH\u0002J\b\u0010\u001e\u001a\u00020\u001fH\u0003J\u0010\u0010 \u001a\u00020\u001f2\u0006\u0010!\u001a\u00020\"H\u0002J\u0018\u0010#\u001a\u00020\u001c2\u0006\u0010$\u001a\u00020\u001c2\u0006\u0010%\u001a\u00020\u001cH\u0002J\b\u0010&\u001a\u00020\u001fH\u0002J\u0010\u0010\'\u001a\u00020\u001f2\u0006\u0010(\u001a\u00020)H\u0016J$\u0010*\u001a\u00020+2\u0006\u0010,\u001a\u00020-2\b\u0010.\u001a\u0004\u0018\u00010/2\b\u00100\u001a\u0004\u0018\u000101H\u0016J\b\u00102\u001a\u00020\u001fH\u0016J\u0010\u00103\u001a\u00020\u001f2\u0006\u00104\u001a\u00020\u0018H\u0016J0\u00105\u001a\u00020\u001f2\u000e\u00106\u001a\n\u0012\u0004\u0012\u000208\u0018\u0001072\u0006\u00109\u001a\u00020:2\u0006\u0010;\u001a\u00020\u001c2\u0006\u0010<\u001a\u00020\u001cH\u0016J\b\u0010=\u001a\u00020\u001fH\u0016J\u001a\u0010>\u001a\u00020\u001f2\u0006\u0010?\u001a\u00020+2\b\u00100\u001a\u0004\u0018\u000101H\u0017J\b\u0010@\u001a\u00020\u001fH\u0002J\b\u0010A\u001a\u00020\u001fH\u0002R\u0010\u0010\u0004\u001a\u0004\u0018\u00010\u0005X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0006\u001a\u00020\u0007X\u0082.\u00a2\u0006\u0002\n\u0000R\u0010\u0010\b\u001a\u0004\u0018\u00010\tX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\n\u001a\u00020\u000bX\u0082.\u00a2\u0006\u0002\n\u0000R\u0010\u0010\f\u001a\u0004\u0018\u00010\rX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u000e\u001a\u00020\u00058BX\u0082\u0004\u00a2\u0006\u0006\u001a\u0004\b\u000f\u0010\u0010R\u0010\u0010\u0011\u001a\u0004\u0018\u00010\u0012X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0013\u001a\u00020\u0014X\u0082.\u00a2\u0006\u0002\n\u0000R\u0010\u0010\u0015\u001a\u0004\u0018\u00010\u0016X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0017\u001a\u00020\u0018X\u0082D\u00a2\u0006\u0002\n\u0000\u00a8\u0006B"}, d2 = {"Lorg/tensorflow/lite/examples/objectdetection/fragments/CameraFragment;", "Landroidx/fragment/app/Fragment;", "Lorg/tensorflow/lite/examples/objectdetection/ObjectDetectorHelper$DetectorListener;", "()V", "_fragmentCameraBinding", "Lorg/tensorflow/lite/examples/objectdetection/databinding/FragmentCameraBinding;", "bitmapBuffer", "Landroid/graphics/Bitmap;", "camera", "Landroidx/camera/core/Camera;", "cameraExecutor", "Ljava/util/concurrent/ExecutorService;", "cameraProvider", "Landroidx/camera/lifecycle/ProcessCameraProvider;", "fragmentCameraBinding", "getFragmentCameraBinding", "()Lorg/tensorflow/lite/examples/objectdetection/databinding/FragmentCameraBinding;", "imageAnalyzer", "Landroidx/camera/core/ImageAnalysis;", "objectDetectorHelper", "Lorg/tensorflow/lite/examples/objectdetection/ObjectDetectorHelper;", "preview", "Landroidx/camera/core/Preview;", "tag", "", "aspectRatio", "Landroid/util/Size;", "width", "", "height", "bindCameraUseCases", "", "detectObjects", "image", "Landroidx/camera/core/ImageProxy;", "gcd", "a", "b", "initBottomSheetControls", "onConfigurationChanged", "newConfig", "Landroid/content/res/Configuration;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "onDestroyView", "onError", "error", "onResults", "results", "", "Lorg/tensorflow/lite/task/vision/detector/Detection;", "inferenceTime", "", "imageHeight", "imageWidth", "onResume", "onViewCreated", "view", "setUpCamera", "updateControlsUi", "app_debug"})
+public final class CameraFragment extends androidx.fragment.app.Fragment implements org.tensorflow.lite.examples.objectdetection.ObjectDetectorHelper.DetectorListener {
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String tag = "ObjectDetection";
+    @org.jetbrains.annotations.Nullable()
+    private org.tensorflow.lite.examples.objectdetection.databinding.FragmentCameraBinding _fragmentCameraBinding;
+    private org.tensorflow.lite.examples.objectdetection.ObjectDetectorHelper objectDetectorHelper;
+    private android.graphics.Bitmap bitmapBuffer;
+    @org.jetbrains.annotations.Nullable()
+    private androidx.camera.core.Preview preview;
+    @org.jetbrains.annotations.Nullable()
+    private androidx.camera.core.ImageAnalysis imageAnalyzer;
+    @org.jetbrains.annotations.Nullable()
+    private androidx.camera.core.Camera camera;
+    @org.jetbrains.annotations.Nullable()
+    private androidx.camera.lifecycle.ProcessCameraProvider cameraProvider;
+    
+    /**
+     * Blocking camera operations are performed using this executor
+     */
+    private java.util.concurrent.ExecutorService cameraExecutor;
+    
+    public CameraFragment() {
+        super();
+    }
+    
+    private final org.tensorflow.lite.examples.objectdetection.databinding.FragmentCameraBinding getFragmentCameraBinding() {
+        return null;
+    }
+    
+    private final android.util.Size aspectRatio(int width, int height) {
+        return null;
+    }
+    
+    private final int gcd(int a, int b) {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    public void onResume() {
+    }
+    
+    @java.lang.Override()
+    public void onDestroyView() {
+    }
+    
+    @java.lang.Override()
+    @org.jetbrains.annotations.NotNull()
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull()
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable()
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    @android.annotation.SuppressLint(value = {"MissingPermission"})
+    public void onViewCreated(@org.jetbrains.annotations.NotNull()
+    android.view.View view, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    private final void initBottomSheetControls() {
+    }
+    
+    private final void updateControlsUi() {
+    }
+    
+    private final void setUpCamera() {
+    }
+    
+    @android.annotation.SuppressLint(value = {"UnsafeOptInUsageError"})
+    private final void bindCameraUseCases() {
+    }
+    
+    private final void detectObjects(androidx.camera.core.ImageProxy image) {
+    }
+    
+    @java.lang.Override()
+    public void onConfigurationChanged(@org.jetbrains.annotations.NotNull()
+    android.content.res.Configuration newConfig) {
+    }
+    
+    @java.lang.Override()
+    public void onResults(@org.jetbrains.annotations.Nullable()
+    java.util.List<org.tensorflow.lite.task.vision.detector.Detection> results, long inferenceTime, int imageHeight, int imageWidth) {
+    }
+    
+    @java.lang.Override()
+    public void onError(@org.jetbrains.annotations.NotNull()
+    java.lang.String error) {
+    }
+}
